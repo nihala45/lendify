7eaadafe7cb0e9328827a0c717c989c152309835
nihala shirin <nihalashirin02@gmail.com>
2025-07-15 23:35:55 +0530

    upload new backned base_url

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,4 +1,6 @@
-export const BASE_URL='http://127.0.0.1:8000/';
+// export const BASE_URL='http://127.0.0.1:8000/';
+
+export const BASE_URL='https://lendify-3ohi.onrender.com';
 export const ACCESS_TOKEN='access_token';
 export const REFRESH_TOKEN='refresh_token';
 export const USERNAME='username';
